c457a375eb75df691c47b6e0edeb5d2fa713110a
Bramasto17 <bram@umroh.com>
2019-10-10 17:24:15 +0700

    edit dashboard

--- resources/views/hrms/dashboard.blade.php
+++ resources/views/hrms/dashboard.blade.php
@@ -37,150 +37,55 @@
                 <div class="panel panel-tile">
                     <div class="panel-body">
                         <div class="row pv10">
-                            <div class="col-xs-5 ph10">
+                            <div class="col-xs-6 col-xs-offset-3 ph10">
+                                <img src="/assets/img/pages/clipart0.png" class="img-responsive mauto" alt=""/></div>
+                            <div class="col-xs-12" style="text-align: center;">
+                                <h3 class="text-muted"><a href="{{route('list-departments')}}"> DEPARTMENT <br> MANAGER</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="panel panel-tile">
+                    <div class="panel-body">
+                        <div class="row pv10">
+                            <div class="col-xs-6 col-xs-offset-3 ph10">
+                                <img src="/assets/img/pages/clipart6.png" class="img-responsive mauto" alt=""/></div>
+                            <div class="col-xs-12" style="text-align: center;">
+                                <h3 class="text-muted"><a href="{{route('list-positions')}}"> POSITION <br> MANAGER</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="panel panel-tile">
+                    <div class="panel-body">
+                        <div class="row pv10">
+                            <div class="col-xs-6 col-xs-offset-3 ph10">
+                                <img src="/assets/img/pages/clipart3.png" class="img-responsive mauto" alt=""/></div>
+                            <div class="col-xs-12" style="text-align: center;">
+                                <h3 class="text-muted"><a href="{{route('list-jobs')}}"> JOB <br> MANAGER</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="panel panel-tile">
+                    <div class="panel-body">
+                        <div class="row pv10">
+                            <div class="col-xs-6 col-xs-offset-3 ph10">
                                 <img src="/assets/img/pages/clipart2.png" class="img-responsive mauto" alt=""/></div>
-                            <div class="col-xs-7 pl5">
-                                <h3 class="text-muted"><a href="{{route('employee-manager')}}"> EMPLOYEE MANAGER</a></h3>
-                                {{--<h2 class="fs50 mt5 mbn">385</h2>--}}
+                            <div class="col-xs-12" style="text-align: center;">
+                                <h3 class="text-muted"><a href="{{route('list-applicants')}}"> APPLICANT <br> MANAGER</a></h3>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-xl-3">
-                <div class="panel panel-tile">
-                    <div class="panel-body">
-                        <div class="row pv10">
-                            <div class="col-xs-5 ph10"><img src="/assets/img/pages/clipart0.png"
-                                                            class="img-responsive mauto" alt=""/></div>
-                            <div class="col-xs-7 pl5">
-                                <h3 class="text-muted"> <a href="{{route('total-leave-list')}}"> LEAVE <br/> MANAGER </a></h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                <div class="col-sm-6 col-xl-3">
-                    <div class="panel panel-tile">
-                        <div class="panel-body">
-                            <div class="row pv10">
-                                <div class="col-xs-5 ph10"><img src="/assets/img/pages/Laptop Sketch-64x64"
-                                                                class="img-responsive mauto" style="height: 100px; width: 100px;" alt=""/></div>
-                                <div class="col-xs-7 pl5">
-                                    <h3 class="text-muted"> <a href="{{route('asset-listing')}}"> ASSET <br /> MANAGER </a></h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-xl-3">
-                    <div class="panel panel-tile">
-                        <div class="panel-body">
-                            <div class="row pv10">
-                                <div class="col-xs-5 ph10"><img src="/assets/img/pages/dollar.jpg"
-                                                                class="img-responsive mauto" style="height: 100px; width: 100px;" alt=""/></div>
-                                <div class="col-xs-7 pl5">
-                                    <h3 class="text-muted"> <a href="{{route('expense-list')}}"> EXPENSE <br /> MANAGER </a></h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            <div class="col-sm-6 col-xl-3">
-                <div class="panel panel-tile">
-                    <div class="panel-body">
-                        <div class="row pv10">
-                            <div class="col-xs-5 ph10"><img src="/assets/img/pages/clipart5.png"
-                                                            class="img-responsive mauto" alt=""/></div>
-                            <div class="col-xs-7 pl5">
-                                <h3 class="text-muted"><a href="{{route('attendance-manager')}}"> ATTENDANCE MANAGER </a></h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
             @endif
-                @if(!Auth::user()->isHR())
-                <div class="col-sm-6 col-xl-3">
-                    <div class="panel panel-tile">
-                        <div class="panel-body">
-                            <div class="row pv10">
-                                <div class="col-xs-5 ph10"><img src="/assets/img/pages/clipart0.png"
-                                                                class="img-responsive mauto" alt=""/></div>
-                                <div class="col-xs-7 pl5">
-                                    <h3 class="text-muted"><a href="{{route('my-leave-list')}}"> LEAVES </a></h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @endif
-
-            <div class="col-sm-6 col-xl-3">
-                <div class="panel panel-tile">
-                    <div class="panel-body">
-                        <div class="row pv10">
-                            <div class="col-xs-5 ph10"><img src="/assets/img/pages/clipart6.png"
-                                                            class="img-responsive mauto" alt=""/></div>
-                            <div class="col-xs-7 pl5">
-                                <h3 class="text-muted"><a href="{{route('hr-policy')}}"> HR POLICIES </a></h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-                @if($events)
-                <div class="col-md-12">
-                    <h3 class="mb10 mr5 notification" data-note-style="primary" style="color: darkturquoise">Latest &nbsp; Events </h3>
-                @foreach (array_chunk($events, 3, true) as $results)
-                    <table class="table">
-                        <tr>
-                            @foreach($results as $event)
-                            <td>
-                                <div class='fc-event fc-event-primary' data-event="primary">
-                                <div class="fc-event-icon" style="color: darkslateblue">
-                                    <span class="fa fa-exclamation"></span>
-                                </div>
-                                <div class="fc-event-desc blink" id="blink">
-                                    <a href="{{route('create-event')}}" ><b>{{ \Carbon\Carbon::createFromTimestamp(strtotime($event->date))->diffForHumans()}} </b> {{$event->name}}</a>
-                                </div>
-                                    </div>
-                            </td>
-                            @endforeach
-                        </tr>
-                    </table>
-                    @endforeach
-               </div>
-                @endif
-
-                @if($meetings)
-                <div class="col-md-12">
-                    <h3 class=" mb10 mr5 notification" data-note-style="primary" style="color: darkturquoise"> Latest &nbsp;&nbsp; Meetings </h3>
-                    @foreach (array_chunk($meetings, 3, true) as $results)
-                        <table class="table">
-                            <tr>
-                                @foreach($results as $meeting)
-                                    <td>
-                                        <div class='fc-event fc-event-primary' data-event="primary">
-                                            <div class="fc-event-icon" style="color: darkslateblue">
-                                                <span class="fa fa-exclamation"></span>
-                                            </div>
-                                            <div class="fc-event-desc blink" id="blink">
-                                                <b>{{ \Carbon\Carbon::createFromTimestamp(strtotime($meeting->date))->diffForHumans()}} </b> {{$meeting->name}}
-                                            </div>
-                                        </div>
-                                    </td>
-                                @endforeach
-                            </tr>
-                        </table>
-                    @endforeach
-                </div>
-                    @endif
-
-
              </div>
            </div>
          </section>

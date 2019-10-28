08cf2b4e11dea1ec80d4dfc07fde568c4c231478
Bramasto17 <bram@umroh.com>
2019-10-28 09:13:27 +0700

    update seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,10 +18,11 @@ class DatabaseSeeder extends Seeder
         $this->call(JobTypesSeeder::class);
         $this->call(DepartmentsSeeder::class);
         $this->call(PositionsSeeder::class);
-        $this->call(JobsSeeder::class);
         $this->call(SkillTypesSeeder::class);
         $this->call(EducationStatusesSeeder::class);
         $this->call(CareerStatusesSeeder::class);
         $this->call(SkillsSeeder::class);
+        $this->call(JobsSeeder::class);
+        $this->call(JobRequirementsSeeder::class);
     }
 }
---
+++ database/seeds/JobRequirementsSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class JobRequirementsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+        \DB::table('job_requirements')->truncate();
+        \DB::table('job_requirements')->insert([
+            [
+                'job_id' => 1,
+                'skill_id' => 25,
+            ],
+            [
+                'job_id' => 1,
+                'skill_id' => 26,
+            ],
+            [
+                'job_id' => 1,
+                'skill_id' => 27,
+            ],
+            [
+                'job_id' => 1,
+                'skill_id' => 32,
+            ],
+        ]);
+        Schema::enableForeignKeyConstraints();
+    }
+}

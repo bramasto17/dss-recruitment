73e49f5b36330f2f1bbed25a46b44deae7011087
Bramasto17 <bram@umroh.com>
2019-10-28 10:17:28 +0700

    update models

---
+++ app/Models/CareerStatuses.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CareerStatuses extends Model
+{
+    use SoftDeletes;
+
+    protected $table      = 'career_statuses';
+	protected $guarded    = array('id');
+	protected $dates      = ['created_at', 'updated_at', 'deleted_at'];
+
+	public function applicant()
+    {
+        return $this->belongsTo('App\Models\Applicants', 'id', 'applicant_id');
+    }
+}

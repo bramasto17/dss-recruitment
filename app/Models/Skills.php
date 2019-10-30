f5791638c0dde0e44bbfded14ef852ffbc97394a
Bramasto17 <bram@umroh.com>
2019-10-30 09:25:01 +0700

    (in progress) validate which skill is required for the job

--- app/Models/Skills.php
+++ app/Models/Skills.php
@@ -18,12 +18,24 @@ class Skills extends Model
         return $this->belongsTo('App\Models\SkillTypes', 'skill_type_id', 'id');
     }
 
+    public function applicantSkills()
+    {
+        return $this->hasMany('App\Models\ApplicantSkills', 'skill_id', 'id');
+    }
+
+    public function jobRequirements()
+    {
+        return $this->hasMany('App\Models\JobRequirements', 'skill_id', 'id');
+    }
+
     public function getGradeAttribute()
     {
     	if($this->skill_type_id == 1) $mul = 1;
     	else if($this->skill_type_id == 2) $mul = 1.5;
     	else if($this->skill_type_id == 3) $mul = 2;
     	else $mul = 0;
+
+        // if($this->id == $this->applicantSkills->applicant->applications->job->requirements->id) $mul = $mul * 3;
 
         return ($this->type->grade * $mul);
     }

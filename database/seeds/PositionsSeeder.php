df2f7ff7f7655616075487a84c05d23d4f813899
Bramasto17 <bram@umroh.com>
2019-11-05 09:21:13 +0700

    update position seeder

--- database/seeds/PositionsSeeder.php
+++ database/seeds/PositionsSeeder.php
@@ -48,7 +48,7 @@ class PositionsSeeder extends Seeder
             ],
             [
                 'department_id' => 5,
-                'name' => 'Design',
+                'name' => 'Engineer Design',
             ],
             [
                 'department_id' => 5,
